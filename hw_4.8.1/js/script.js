cd6e12c82c1357bbc0769481f0ed3d77be6e0bba
darinaavtaeva <55583670+darinaavtaeva@users.noreply.github.com>
2020-02-13 14:24:43 +0300

    .

--- hw_4.8.1/js/script.js
+++ hw_4.8.1/js/script.js
@@ -1,8 +1,8 @@
-let money = prompt('Сколько у вас денег?');
-let apple = prompt('Сколько вы купили яблок?');
-let bread = prompt('Сколько вы купили батонов хлеба?');
-let applePrice = prompt('Сколько стоит одно яблоко?') * apple;
-let breadPrice = prompt('Сколько стоит один батон хлеба?') * bread;
+let money = parseFloat(prompt('Сколько у вас денег?'));
+let apple = parseFloat(prompt('Сколько вы купили яблок?'));
+let bread = parseFloat(prompt('Сколько вы купили батонов хлеба?'));
+let applePrice = parseFloat(prompt('Сколько стоит одно яблоко?') * apple);
+let breadPrice = parseFloat(prompt('Сколько стоит один батон хлеба?') * bread);
 let total = applePrice+breadPrice <= money;
 
 function haveEnough(money, apple, bread, applePrice, breadPrice, total) {

39feab831106badb1a1571c03c99746ddd2bfacf
darinaavtaeva <55583670+darinaavtaeva@users.noreply.github.com>
2020-02-27 11:30:39 +0300

    Update script.js

--- hw_6.4.1/js/script.js
+++ hw_6.4.1/js/script.js
@@ -5,12 +5,11 @@ const form = document.forms[0];
 form.onsubmit = function(e){
 	e.preventDefault();
 
-	var cityName = form.elements.cityName.value;
-	var url = 'http://api.openweathermap.org/data/2.5/weather?q='+form.elements.cityName.value+'&appid='+APIKey;
-	var insertTemp = document.querySelector('.insertTemp');
-	var insertWind = document.querySelector('.insertWind');
+	let url = `http://api.openweathermap.org/data/2.5/weather?q=${form.elements.cityName.value}&appid=${APIKey}`;
+	let insertTemp = document.querySelector('.insertTemp');
+	let insertWind = document.querySelector('.insertWind');
 
-	var xhr = new XMLHttpRequest();
+	let xhr = new XMLHttpRequest();
 	xhr.open('GET', url, false);
 	xhr.send();
 
